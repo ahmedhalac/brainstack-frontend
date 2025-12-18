62fe0fba9bc971d2c6732afb0c7b40e6fe143a4b
ahmedhalac <ahmedhalac@outlook.com>
2025-12-18 23:28:59 +0100

    Added lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,30 @@
 import { Routes } from '@angular/router';
-import { Login } from './features/auth/login/login';
-import { Register } from './features/auth/register/register';
-import { VerifyEmail } from './features/auth/verify-email/verify-email';
-import { Dashboard } from './features/dashboard/dashboard';
 import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
-  { path: 'login', component: Login },
-  { path: 'register', component: Register },
-  { path: 'verify-email', component: VerifyEmail },
-  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/register/register').then((m) => m.Register),
+  },
+  {
+    path: 'verify-email',
+    loadComponent: () =>
+      import('./features/auth/verify-email/verify-email').then(
+        (m) => m.VerifyEmail,
+      ),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard').then((m) => m.Dashboard),
+    canActivate: [authGuard],
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, MatButtonModule, RouterModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  router = inject(Router);
   protected readonly title = signal('brainstack-frontend');
-
-  // onLogin(): void {
-  //   this.router.navigate(['/login']);
-  // }
-
-  // onRegister(): void {
-  //   this.router.navigate(['/register']);
-  // }
 }
